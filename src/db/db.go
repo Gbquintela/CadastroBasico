1a9266c39cfb82bc036d05351573f6c48c1d8e8f
Gabriel Quintela <168236388+Gbquintela@users.noreply.github.com>
2024-11-05 14:43:50 -0300

    Update db.go

--- src/db/db.go
+++ src/db/db.go
@@ -9,7 +9,7 @@ var DB *sql.DB
 
 func InitDB() {
 	var err error
-	// Altere os detalhes da string de conexão para o seu banco de dados
+	
 	DB, err = sql.Open("mysql", "root:Mafioso55@@tcp(127.0.0.1:3306)/cadastrosimples")
 	if err != nil {
 		log.Fatal(err)
